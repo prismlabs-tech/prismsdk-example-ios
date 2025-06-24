77406b9fd1aa3fa0eeefd1eb1e22921fd07dcea9
Ivan Banic <ivan@prismlabs.tech>
2025-06-24 10:54:20 +0200

    Minor fixes

--- PrismExampleApp/Sources/Coordinators/ScanScreen/NewScan/ScanCard/NewScanWeightView.swift
+++ PrismExampleApp/Sources/Coordinators/ScanScreen/NewScan/ScanCard/NewScanWeightView.swift
@@ -1,9 +1,10 @@
-//
-//  NewScanWeightView.swift
-//  PrismReference
-//
-//  Created by Anthony Castelli on 2/14/23.
-//
+/*
+ * Copyright (c) Prismlabs, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 import SwiftUI
 
